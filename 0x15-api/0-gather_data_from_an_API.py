c07161eead5cee512be24a96730cacd62d4d0d22
Benita Michael <benubiaka@gmail.com>
2024-01-02 14:05:10 -0800

    Task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,12 @@
 #!/usr/bin/python3
-''' Python script that, using this REST API, for a given employee
-ID, returns information about his/her TODO list progress
-'''
-import re  # Import the regular expression module
-import requests  # Import the requests module
-import sys  # Import the sys module
-
-# The API's URL
-API_URL = 'https://jsonplaceholder.typicode.com'
-
-# Check if the script is being run directly
-if __name__ == '__main__':
-    # Check if command line arguments were provided
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            # Get the user's information from the API
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
-            todos_res = requests.get('{}/todos'.format(API_URL)).json()
-            user_name = user_res.get('name')
-
-            # Filter todos for the given user
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
-
-            # Print the user's tasks
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+"""script returns todo list information for a given employee ID."""
+import requests
+import sys
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    todo_done = [t.get('title') for t in todo if t.get('completed') is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(todo_done), len(todo)))
+    [print("\t {}".format(c)) for c in todo_done]
